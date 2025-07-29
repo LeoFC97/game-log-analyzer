293fa202b67e797f123fce8adfd061a5e02ef0aa
Leonardo <Leonardo.fc2@outlook.com>
2025-07-29 14:06:01 -0300

    add missing fields

--- src/entities/kill.entity.ts
+++ src/entities/kill.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { Match } from './match.entity';
 
 @Entity()
@@ -18,6 +18,10 @@ export class Kill {
   @Column()
   timestamp: Date;
 
-  @ManyToOne(() => Match, match => match.kills)
+  @ManyToOne(() => Match, match => match.kills, { nullable: false })
+  @JoinColumn({ name: 'matchId' })
   match: Match;
+
+  @Column()
+  matchId: number;
 }
